50db61dd7eed398c60e5b02a527d42bf0e672a88
Aleksander <aandreyko@more.tv>
2024-02-19 17:48:21 +0400

    SSE handlers

--- web/src/pages/Lobby/Lobby.jsx
+++ web/src/pages/Lobby/Lobby.jsx
@@ -1,20 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { eventTypes, stratGameBoard } from '../../shared/constants';
-import { endpoints } from '../../shared/constants';
+import { endpoints, eventTypes, gameBoard } from '../../shared/constants';
 
 import { GameButton } from '../../components/GameButton/GameButton';
 
 import './Lobby.css';
 
 export const Lobby = () => {
+  const [lobbyStarted, setLobbyStarted] = useState(false);
+  const [xPlayer, setXPlayer] = useState(false);
+  const [isMyTurn, setIsMyTurn] = useState(false);
+
   const { uid } = useParams();
+
+  // TODO: clsx
+  let classNames = 'lobby-wrapper';
+
+  if (!isMyTurn) {
+    classNames += ' locked';
+  }
 
   useEffect(() => {
     const eventSource = new EventSource(`${endpoints.events}/${uid}`);
 
     eventSource.addEventListener(eventTypes.start, (event) => {
+      console.log(event);
+      const data = JSON.parse(event.data);
+
+      setLobbyStarted(true);
+      setXPlayer(data.xPlayer);
+      setIsMyTurn(data.xPlayer);
+    });
+
+    eventSource.addEventListener(eventTypes.turn, (event) => {
       console.log(event);
     });
 
@@ -24,10 +43,14 @@ export const Lobby = () => {
   }, [uid]);
 
   return (
-    <div className="lobby-wrapper">
-      {stratGameBoard.map(({ id, field }) => (
-        <GameButton key={id} name={field} />
-      ))}
+    <div className={classNames}>
+      {!lobbyStarted && <div>waiting for players</div>}
+      {isMyTurn && <div>make a turn</div>}
+      <div className="board">
+        {gameBoard.map(({ id, field }) => (
+          <GameButton key={id} name={field} />
+        ))}
+      </div>
     </div>
   );
 };
--- web/src/shared/constants.js
+++ web/src/shared/constants.js
@@ -5,16 +5,17 @@ export const endpoints = {
 
 export const eventTypes = {
   start: 'start',
+  turn: 'turn',
 };
 
-export const startGameBoard = [
-  { id: 1, field: 'e' },
-  { id: 2, field: 'e' },
-  { id: 3, field: 'e' },
-  { id: 4, field: 'e' },
-  { id: 5, field: 'e' },
-  { id: 6, field: 'e' },
-  { id: 7, field: 'e' },
-  { id: 8, field: 'e' },
-  { id: 9, field: 'e' },
+export const gameBoard = [
+  { id: 1, field: 'e', position: [0, 0] },
+  { id: 2, field: 'e', position: [1, 0] },
+  { id: 3, field: 'e', position: [2, 0] },
+  { id: 4, field: 'e', position: [0, 1] },
+  { id: 5, field: 'e', position: [1, 1] },
+  { id: 6, field: 'e', position: [2, 1] },
+  { id: 7, field: 'e', position: [0, 2] },
+  { id: 8, field: 'e', position: [1, 2] },
+  { id: 9, field: 'e', position: [2, 2] },
 ];
