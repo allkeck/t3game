49afed3731492c019662a8075256cfadb9ec48db
Ivan Andreyko <myromeu@ya.ru>
2024-02-23 21:06:28 +0400

    change bitwise assignment to logical

--- web/src/pages/Lobby/Lobby.jsx
+++ web/src/pages/Lobby/Lobby.jsx
@@ -80,10 +80,10 @@ export const Lobby = () => {
     let secondaryDiagFinished = (row + col) === 2;
     for (let cell of currentGameBoard) {
       let [cellRow, cellCol] = cell.position;
-      if (cellRow === row) rowFinished &= cell.value === player;
-      if (cellCol === col) colFinished &= cell.value === player;
-      if (cellRow === cellCol) mainDiagFinished &= cell.value === player;
-      if ((cellRow+cellCol) === 2) secondaryDiagFinished &= cell.value === player;
+      if (cellRow === row) rowFinished &&= cell.value === player;
+      if (cellCol === col) colFinished &&= cell.value === player;
+      if (cellRow === cellCol) mainDiagFinished &&= cell.value === player;
+      if ((cellRow+cellCol) === 2) secondaryDiagFinished &&= cell.value === player;
     }
     return rowFinished || colFinished || mainDiagFinished || secondaryDiagFinished;
   };
