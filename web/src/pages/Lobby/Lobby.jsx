04fe0ccb85b2bcc92bb1e93b27d8f1b3663d563a
Ivan Andreyko <myromeu@ya.ru>
2024-02-23 20:59:25 +0400

    v2 check finish

--- web/src/pages/Lobby/Lobby.jsx
+++ web/src/pages/Lobby/Lobby.jsx
@@ -71,7 +71,22 @@ export const Lobby = () => {
     );
   };
 
-  const isBoardFinished = (row, col, player) => {};
+  const isBoardFinished = (row, col, player) => {}
+
+  const isBoardFinishedV2 = (row, col, player) => {
+    let rowFinished = true;
+    let colFinished = true;
+    let mainDiagFinished = row === col;
+    let secondaryDiagFinished = (row + col) === 2;
+    for (let cell of currentGameBoard) {
+      let [cellRow, cellCol] = cell.position;
+      if (cellRow === row) rowFinished &= cell.value === player;
+      if (cellCol === col) colFinished &= cell.value === player;
+      if (cellRow === cellCol) mainDiagFinished &= cell.value === player;
+      if ((cellRow+cellCol) === 2) secondaryDiagFinished &= cell.value === player;
+    }
+    return rowFinished || colFinished || mainDiagFinished || secondaryDiagFinished;
+  };
 
   console.log(currentGameBoard);
 
